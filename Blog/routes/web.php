7f6a04402cf37e12184953c6e86a2846d1385c8a
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-05-25 22:13:07 +0630

    -

--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -5,26 +5,6 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\CommentController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/articles', function() {
-//     return 'Article List';
-// });
-
-// Route::get('/articles/details', function() {
-//     return 'Article Detail';
-// })->name('article.detail');
-
-// Route::get('/articles/details/{id}', function($id) {
-//     return "Article Detail - $id";
-// });
-
-// Route::get('/articles/more', function() {
-//     return redirect()->route('article.detail');
-// });
-
 Route::get('/', [ArticleController::class, 'index']);
 
 Route::get('/articles', [ArticleController::class, 'index']);
